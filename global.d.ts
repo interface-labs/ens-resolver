5ae755b92117d7b04270cf28a94fc35074d33995
Nikita Kudryavtsev <kud.nikita@gmail.com>
2022-04-23 19:36:25 +0400

    updated build

---
+++ global.d.ts
@@ -0,0 +1,11 @@
+
+namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: string;
+    PORT: string;
+    INFURA_KEY: string;
+    IPFS_GATEWAY: string;
+  }
+}
+
+
--- src/main.ts
+++ src/main.ts
@@ -5,30 +5,29 @@ import { AvatarResolver, utils as avtUtils } from '@ensdomains/ens-avatar'
 import { JSDOM } from 'jsdom'
 
 const jsdom = new JSDOM().window
-
 const provider = new StaticJsonRpcProvider(`https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`)
+const PORT = process.env.PORT
 
 const app = express()
-
 // https://www.larvalabs.com/blog/2021-8-18-18-0/on-chain-cryptopunks
 
-app.use("/:handle", async (req, res, next) => {
+app.get("/:handle", async (req, res) => {
   const { handle } = req.params
   if (!handle?.endsWith('.eth')) {
-    return res.status(400).send("missing or invalid ens handle")
+    return res.status(404).send("missing or invalid ens handle")
   }
   console.log(handle)
   const resolver = new AvatarResolver(provider, { ipfs: process.env.IPFS_GATEWAY })
 
   return resolver.getAvatar(handle, { jsdomWindow: jsdom }).then(avatar => {
-    console.log(handle, "->", avatar)
-    return res.status(200).json(avatar)
+    console.log(Date.now(), "-> ", handle, "-> ", avatar)
+    return res.status(200).json(avatar?.replace('https://ipfs.io', process.env.IPFS_GATEWAY))
   }).catch(err => {
-    console.log('error.resolver.getAvatar')
+    console.log('error.resolver.getAvatar', handle, err)
     return res.status(500).send(err)
   })
 })
 
-app.listen(process.env.APP_PORT, () => {
-  console.log(`server is up and running at port: ${process.env.APP_PORT}`)
+app.listen(PORT, () => {
+  console.log(`🦄 [server]: is running at ${PORT}`)
 })
