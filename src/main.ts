87bac91ef2476ec90dbdd5dda3709483e531d563
Nikita Kudryavtsev <kud.nikita@gmail.com>
2022-05-26 23:29:21 +0400

    add base64 support

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import express = require("express")
 import { StaticJsonRpcProvider } from '@ethersproject/providers'
 import { AvatarResolver, utils as avtUtils } from '@ensdomains/ens-avatar'
 import { JSDOM } from 'jsdom'
+import sharp from 'sharp';
+import { Contract } from '@ethersproject/contracts';
+import { NFTStorage, Blob } from 'nft.storage';
+
+export const storage = new NFTStorage({ token: process.env.NFT_STORAGE_API_KEY || "" });
 
 const jsdom = new JSDOM().window
 const provider = new StaticJsonRpcProvider(`https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`)
@@ -19,8 +24,20 @@ app.get("/:handle", async (req, res) => {
   console.log(handle)
   const resolver = new AvatarResolver(provider, { ipfs: process.env.IPFS_GATEWAY })
 
-  return resolver.getAvatar(handle, { jsdomWindow: jsdom }).then(avatar => {
+  return resolver.getAvatar(handle, { jsdomWindow: jsdom }).then(async avatar => {
     console.log(Date.now(), "-> ", handle, "-> ", avatar)
+
+    if (avatar?.startsWith("data:image/svg+xml;base64,")) {
+
+      const svg = Buffer.from(avatar.substring(26), 'base64');
+      const png = await sharp(svg).png().toBuffer()
+      const imageCID = await storage.storeBlob(new Blob([png]))
+
+      if (imageCID) {
+        avatar = `${process.env.IPFS_GATEWAY}/ipfs/${imageCID}`
+      }
+    }
+
     return res.status(200).json(avatar?.replace('https://ipfs.io', process.env.IPFS_GATEWAY))
   }).catch(err => {
     console.log('error.resolver.getAvatar', handle, err)
